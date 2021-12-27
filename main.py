2fcfa7ce0908cbcda6bd38cad2c0ceca7a46b79b
Gus-The-Forklift-Driver <benhubledingue@gmail.com>
2021-12-27 12:23:11 +0100

    added coinmarket cap api

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ import random
 
 import scrollphathd
 
+from coinmarketcapapi import CoinMarketCapAPI, CoinMarketCapAPIError
+from secret import coinMarketCapApiKey
+cmc = CoinMarketCapAPI(coinMarketCapApiKey)
+
+r = cmc.cryptocurrency_quotes_latest(symbol='BTC')
+
+display = f"BTC  24h change = {r.data['volume_change_24h']}, current price = {r.data['price']}"
+
 scrollphathd.write_string(" Hello World!", brightness=0.5)
 
 while True:
