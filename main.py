a14f31cccc894dfda744311c0407c4c5463d5fed
Gus-The-Forklift-Driver <benhubledingue@gmail.com>
2021-12-27 15:03:17 +0100

    added async updates

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import math
 import time
 import random
 import utils
+
+import sched
+import time
+
 import scrollphathd
-from scrollphathd.fonts import font3x5
+from scrollphathd.fonts import font3x5, font5x7smoothed
+
 
 from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
@@ -12,15 +17,25 @@ cg = CoinGeckoAPI()
 scrollphathd.fill(0)
 scrollphathd.show()
 
-r = cg.get_price(ids='bitcoin', vs_currencies='gbp')
-display = f"{utils.human_format(r['bitcoin']['gbp'])} "
-scrollphathd.write_string(display, font=font3x5, brightness=0.2)
+s = sched.scheduler(time.time, time.sleep)
 
-# asdfasdf
-while True:
+
+def update_price(sc):
+    print("fetching data")
+    r = cg.get_price(ids='bitcoin', vs_currencies='gbp')
+    display = f"{utils.human_format(r['bitcoin']['gbp'])} "
+    scrollphathd.write_string(display, brightness=0.2)
+    s.enter(60, 1, update_price, (sc,))
+
+
+def scroll(sc):
     # Show the buffer
     scrollphathd.show()
     # Scroll the buffer content
     scrollphathd.scroll()
-    # Wait for 0.1s
-    time.sleep(0.1)
+    s.enter(0.2, 1, scroll, (sc,))
+
+
+s.enter(60, 1, update_price, (s,))
+s.enter(0.2, 1, scroll, (s,))
+s.run()
